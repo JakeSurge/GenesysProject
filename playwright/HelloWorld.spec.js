ee07fe323b60d7d7a2650566dbbf2ad95d4d826a
Jake Surgeon <jakobsurge@gmail.com>
2023-12-01 14:16:37 -0500

    Changed the HelloWorld example so it grabs the text content from the header instead of the header itself and then checks if it equal to the right thing. Removed the 'await' from the 'expect' line since the expect is just comparing strings now.

--- playwright/HelloWorld.spec.js
+++ playwright/HelloWorld.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require("@playwright/test");
 test("WikiPediaPagehasHelloWorldTitle", async ({ page }) => {
     await page.goto("https://en.wikipedia.org/wiki/%22Hello,_World!%22_program");
     //createalocator
-    let header = page.locator(".mw-page-title-main");
+    let header = page.locator(".mw-page-title-main").textContent;
     //Expectheadertohavecorrecttext
-    (await expect(header).textContent) == '"Hello,World!"program';
+    expect(header == '"Hello,World!"program');
 });
