8e16ca743aa1f439403ca6a0b510eede0a9d5198
Jake Surgeon <jakobsurge@gmail.com>
2023-12-02 12:51:54 -0500

    I made my first actual practice test in InterviewTest.spec.js to practice some before attempting the required tests. I also got the HelloWorld.spec.js to work now with .toHaveText. I am not exactly sure why it is working now but I think it is a matter of that I adjusted the string it was comparing it to, to the correct value.

--- playwright/HelloWorld.spec.js
+++ playwright/HelloWorld.spec.js
@@ -1,8 +1,9 @@
 const { test, expect } = require("@playwright/test");
+
 test("WikiPediaPagehasHelloWorldTitle", async ({ page }) => {
     await page.goto("https://en.wikipedia.org/wiki/%22Hello,_World!%22_program");
-    //createalocator
-    let header = page.locator(".mw-page-title-main").textContent;
-    //Expectheadertohavecorrecttext
-    expect(header == '"Hello,World!"program');
+    //create a locator
+    let header = page.locator(".mw-page-title-main");
+    //Expect header to have correct text
+    await expect(header).toHaveText('"Hello, World!" program');
 });
---
+++ playwright/InterviewTest.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("Example Test", async ({ page }) => {
+    //go to initial page
+    await page.goto("https://playwright.dev/docs/writing-tests");
+
+    //set locator for link on page
+    let click = page.getByRole("link", { name: "locator.click()" });
+
+    //click the link
+    await click.click();
+
+    //test for url change
+    await expect(page.url()).toEqual("https://playwright.dev/docs/api/class-locator#locator-click");
+});
