1e9d721c25e082dfb279c2220cc1e7f219dec625
Jake Surgeon <jakobsurge@gmail.com>
2023-12-03 20:45:56 -0500

    Got the CompletePurchase test working by going through the whole purchase process and then confirming that the purchase went through by double checking the order code on the order page. If it matches the order went through.

--- playwright/CompletePurchase.spec.js
+++ playwright/CompletePurchase.spec.js
@@ -59,23 +59,87 @@ test("CanCompletePurchase", async ({ page }) => {
     //add to cart
     await page.getByRole("button", { name: "Add to Cart" }).click();
 
+    //check for the current item count
+    //let itemCount = page.locator(".count").innerText();
+
     //open the cart menu
     await page.getByRole("link", { name: " My Cart 1 1\nitems" }).click();
 
     //click the proceed to cart button
     await page.getByRole("button", { name: "Proceed to Checkout" }).click();
 
+    //wait for the url
+    await page.waitForURL("https://magento.softwaretestingboard.com/checkout/#shipping");
+
+    //check label to make sure it is the right page
+    await expect(page.getByText("Shipping Methods")).toBeVisible();
+
+    //click the new address button
     await page.getByRole("button", { name: "+New Address" }).click();
+
+    //make sure it brought up address input
+    await expect(page.getByRole("heading", { name: "Shipping Address" })).toBeVisible();
+
+    //make sure the address inputs are visible
+    await expect(page.getByLabel("Street Address: Line 1")).toBeVisible();
+
+    //fill in information for address and phone number
     await page.getByLabel("Street Address: Line 1").fill("1100 E 5th St");
     await page.getByLabel("City").fill("Anderson");
     await page.locator('select[name="region_id"]').selectOption("24");
     await page.getByLabel("Zip/Postal Code").fill("46012");
     await page.getByLabel("Phone Number").fill("5133843088");
+
+    //uncheck the save in address book button
     await page.getByLabel("Save in address book").uncheck();
-    await page.getByRole("button", { name: "Ship here" }).click();
+
+    //click ship here to proceed
+    await page.getByRole("button", { name: "Ship here", exact: true }).click();
+
+    //make sure the address inputs are gone
+    await expect(page.getByRole("heading", { name: "Shipping Address" })).not.toBeVisible();
+
+    //select the shipping rate
     await page.getByLabel("Table Rate").check();
+
+    //click next to proceed
     await page.getByRole("button", { name: "Next" }).click();
+
+    //check for place order button
+    const placeOrder = page.getByRole("button", { name: "Place Order" });
+
+    //make sure the place order button is visible
+    await expect(placeOrder).toBeVisible();
+
+    //select your shipping option
     await page.getByLabel("My billing and shipping").check();
-    await page.getByRole("button", { name: "Place Order" }).click();
-    //await page.getByRole('link', { name: '000036394' }).click();
+
+    //click the place order button
+    await placeOrder.click();
+
+    //wait for the url
+    await page.waitForURL("https://magento.softwaretestingboard.com/checkout/onepage/success/");
+
+    //double check it is the right page
+    await expect(page.locator(".base")).toHaveText("Thank you for your purchase!");
+
+    //wait for selector to load in
+    await page.waitForSelector(".order-number");
+
+    //find the order link to verify the order
+    await page.locator(".order-number").click();
+
+    //wait for new url
+    await page.waitForURL("https://magento.softwaretestingboard.com/sales/order/view/order_id/**");
+
+    //find the code of the order with the url
+    //it is always 1 more than the url code
+    const orderCode = parseInt(page.url().slice(67, 72)) + 1;
+    console.log(orderCode);
+
+    //wait for selector to load in
+    await page.waitForSelector(".base");
+
+    //confirm the title text to the expected text
+    await expect(page.locator(".base")).toHaveText("Order # 0000" + orderCode.toString());
 });
