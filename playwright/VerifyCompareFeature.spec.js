941ef4062d9fccd042025321db8756bd9b7fb4ce
Jake Surgeon <jakobsurge@gmail.com>
2023-12-03 23:22:39 -0500

    Got the last required test VerifyCompareFeature fully functional. All the required tests should be full functional now. The only issue is running the tests that add an item to the cart with items already in the cart on the account or running the test across all browser options at once.

--- playwright/VerifyCompareFeature.spec.js
+++ playwright/VerifyCompareFeature.spec.js
@@ -38,7 +38,87 @@ test.beforeEach("UserCanLogin", async ({ page }) => {
     await expect(page.getByRole("banner").getByText("Welcome, Jakob Surgeon!")).toBeVisible();
 });
 
-//tests completing a purchase after adding it to the cart
+//tests the compare item feature on the site (this may have issues if items are already in the comparison list)
 test("CanCompareItems", async ({ page }) => {
-    
+    //go to item
+    await page.getByRole("link", { name: "Radiant Tee" }).first().click();
+
+    //wait for URL
+    await page.waitForURL("https://magento.softwaretestingboard.com/radiant-tee.html");
+
+    //check for add to compare button
+    const addToCompare = page.getByRole("link", { name: " Add to Compare" });
+
+    //make sure add to compare has expected text
+    await expect(addToCompare).toHaveText("Add to Compare");
+
+    //add item to compare list
+    await addToCompare.click();
+
+    //go home
+    await page.getByRole("link", { name: "Home" }).click();
+
+    //wait for url
+    await page.waitForURL("https://magento.softwaretestingboard.com/");
+
+    //go to item
+    await page.getByRole("link", { name: "Fusion Backpack" }).first().click();
+
+    //wait for URL
+    await page.waitForURL("https://magento.softwaretestingboard.com/fusion-backpack.html");
+
+    //make sure add to compare has expected text
+    await expect(addToCompare).toHaveText("Add to Compare");
+
+    //add item to compare list
+    await addToCompare.click();
+
+    //go to comparison list
+    await page.getByRole("link", { name: "comparison list" }).click();
+
+    //wait for URL
+    await page.waitForURL("https://magento.softwaretestingboard.com/catalog/product_compare/");
+
+    //test that the correct information should be in the comparison list. Check for the title, description, and SKU
+    //title
+    await expect(page.getByText("Fusion Backpack", { exact: true })).toBeVisible();
+    await expect(page.getByText("Radiant Tee", { exact: true })).toBeVisible();
+    //description
+    await expect(page.getByText("With the Fusion Backpack")).toBeVisible();
+    await expect(page.getByText("So light and comfy, you'll")).toBeVisible();
+    //SKU
+    await expect(page.getByText("-MB02")).toBeVisible();
+    await expect(page.getByText("WS12")).toBeVisible();
+
+    //remove the closest item (fusion backpack)
+    await page.getByRole("link", { name: " Remove Product" }).first().click();
+
+    //make sure the confirmation is visible
+    await expect(page.getByRole("button", { name: "OK" })).toBeVisible();
+
+    //click the button
+    await page.getByRole("button", { name: "OK" }).click();
+
+    //test for exected information there and now not there
+    //title
+    await expect(page.getByText("Fusion Backpack", { exact: true })).not.toBeVisible();
+    await expect(page.getByText("Radiant Tee", { exact: true })).toBeVisible();
+    //description
+    await expect(page.getByText("With the Fusion Backpack")).not.toBeVisible();
+    await expect(page.getByText("So light and comfy, you'll")).toBeVisible();
+    //SKU
+    await expect(page.getByText("-MB02")).not.toBeVisible();
+    await expect(page.getByText("WS12")).toBeVisible();
+
+    //remove the other item
+    await page.getByRole("link", { name: " Remove Product" }).first().click();
+
+    //make sure the confirmation is visible
+    await expect(page.getByRole("button", { name: "OK" })).toBeVisible();
+
+    //click the button
+    await page.getByRole("button", { name: "OK" }).click();
+
+    //test for no items in comparison list pop up
+    await expect(page.getByText("You have no items to compare.")).toBeVisible();
 });
